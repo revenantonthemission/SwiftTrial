3b757f97f8bfa37e8cf2ee0a51116f2b955a8a25
revenantonthemission <corpseonthemission@icloud.com>
2024-05-21 17:03:48 +0900

    Initial Commit / 새로운 프로젝트를 생성한 후 기존 레포와 통합. 아직 XCode 사용이 익숙하지 않은 상태.

---
+++ SwiftTrial/SwiftTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTrialApp.swift
+//  SwiftTrial
+//
+//  Created by 조준희 on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
